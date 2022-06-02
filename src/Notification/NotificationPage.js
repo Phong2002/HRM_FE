705063657802a0b58c1c8f8c68a8c4fdd71fe46d
Phong2002 <58001398+Phong2002@users.noreply.github.com>
2022-06-03 01:41:19 +0700

    Update NotificationPage.js

--- src/Notification/NotificationPage.js
+++ src/Notification/NotificationPage.js
@@ -48,7 +48,7 @@ const [contentToUser,setContentToUser] = useState("")
 
 // filter 
 
-const [paging,setPaging] = useState();
+const [paging,setPaging] = useState(1);
 
 
 const [totalPages,setTotalPages] = useState(0);
